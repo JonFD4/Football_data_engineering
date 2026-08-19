dd47ecb1b937169b7d0bfe8c90850279391d65f1
JonFD4 <jonathanforsondanso@gmail.com>
2026-08-19 23:08:08 +0100

    Initial set up for main.py

---
+++ config.py
@@ -0,0 +1,9 @@
+DB_config = {
+    "host": "MYSQL_HOST",
+    "port": "MYSQL_PORT",
+    "user": "MYSQL_USER",
+    "password": "MYSQL_PASSWORD",
+    "database": "MYSQL_DATABASE_NAME"
+}
+SEASON = 2024
+LEAGUE_ID = 39
